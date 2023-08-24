c45e8e419c7c07550520e4c7b99da7d6f4fc03e8
Callie Giles <anchoredinthecrown@gmail.com>
2023-08-24 18:04:45 -0400

    enable CORS in router paths

--- src/plants/plants.router.js
+++ src/plants/plants.router.js
@@ -1,18 +1,42 @@
 const router = require("express").Router();
 const controller = require("./plants.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
 
-router
-  .route("/:plantId")
-  .get(controller.read)
-  .put(controller.update)
-  .delete(controller.delete)
-  .all(methodNotAllowed);
+// Set up CORS options
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
+
+let currentId = 1;
+
+function nextId() {
+  const id = currentId.toString();
+  currentId++;
+  return id;
+}
+
+router.route("/").options(cors(corsOptions));
 
 router
   .route("/")
-  .get(controller.list)
-  .post(controller.create)
+  .get(cors(corsOptions), controller.list)
+  .post(cors(corsOptions), controller.create)
+  .put(cors(corsOptions), (req, res) => {
+    res.status(405).json({ error: "Method Not Allowed" }); // Send JSON response with error
+  })
+  .all(methodNotAllowed);
+
+router
+  .route("/:plantId")
+  .get(cors(corsOptions), controller.read)
+  .put(cors(corsOptions), controller.update)
+  .delete(cors(corsOptions), controller.delete)
+  .post(cors(corsOptions), (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(405).send();
+  })
   .all(methodNotAllowed);
 
 module.exports = router;
--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,18 +1,38 @@
-const router = require("express").Router();
+const express = require("express");
+const cors = require("cors");
 const controller = require("./users.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const corsOptions = {
+  origin: "*",
+};
+
+const router = express.Router();
+
+router
+  .route("/")
+  .options((req, res) => {
+    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(204).end();
+  })
+  .get(cors(corsOptions), controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 
 router
   .route("/:userId")
-  .get(controller.read)
+  .options((req, res) => {
+    res.header("Access-Control-Allow-Methods", "GET"); // Only allow GET method in OPTIONS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(204).end();
+  })
+  .get((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+  }, controller.read)
   .put(controller.update)
   .delete(controller.delete)
   .all(methodNotAllowed);
 
-router
-  .route("/")
-  .get(controller.list)
-  .post(controller.create)
-  .all(methodNotAllowed);
 
 module.exports = router;
